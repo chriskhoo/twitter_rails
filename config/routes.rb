ac15abf7984bebae5f8561227f12758fbeab72d5
Christopher Khoo <khooweiwen@gmail.com>
2016-07-25 16:13:07 +0800

    update router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root #controller name # method
+
+
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
 end
